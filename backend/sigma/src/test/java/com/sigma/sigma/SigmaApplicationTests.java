cfffb12806ac9c293e2cf42bc9f46388552d02b5
Aitor Recio Pérez <aitrecper@gmail.com>
2023-04-06 15:08:37 +0200

    First commit

---
+++ backend/sigma/src/test/java/com/sigma/sigma/SigmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigma.sigma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
